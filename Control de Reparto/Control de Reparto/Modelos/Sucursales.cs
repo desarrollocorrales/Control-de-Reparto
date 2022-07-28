e5bdbed7ca8b9371a15ef03dee8677597ad7aa28
desarrollocorrales <desarrollo@loscorrales.com.mx>
2022-07-28 11:09:28 -0500

    Add files via upload

---
+++ Control de Reparto/Control de Reparto/Modelos/Sucursales.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Control_de_Reparto.Modelos
+{
+    public class Sucursales
+    {
+        public int id_sucursal { set; get; }
+        public string descripcion { set; get; }
+        public int seleccionado { get; set; }
+    }
+}
